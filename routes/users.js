60435175ae3849fd6471d4212aaa9767a6b9bed2
sygonsky00 <sygonsky@naver.com>
2020-08-12 11:28:51 +0900

    final commit

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
-
-// Index // 1
-router.get('/', function(req, res){
-  User.find({})
-    .sort({username:1})
-    .exec(function(err, users){
-      if(err) return res.json(err);
-      res.render('users/index', {users:users});
-    });
-});
+var util = require('../util');
 
 // New
 router.get('/new', function(req, res){
@@ -26,7 +17,7 @@ router.post('/', function(req, res){
   User.create(req.body, function(err, user){
     if(err){
       req.flash('user', req.body);
-      req.flash('errors', parseError(err));
+      req.flash('errors', util.parseError(err));
       return res.redirect('/users/new');
     }
     res.redirect('/users');
@@ -34,7 +25,7 @@ router.post('/', function(req, res){
 });
 
 // show
-router.get('/:username', function(req, res){
+router.get('/:username', util.isLoggedin, checkPermission, function(req, res){
   User.findOne({username:req.params.username}, function(err, user){
     if(err) return res.json(err);
     res.render('users/show', {user:user});
@@ -42,7 +33,7 @@ router.get('/:username', function(req, res){
 });
 
 // edit
-router.get('/:username/edit', function(req, res){
+router.get('/:username/edit', util.isLoggedin, checkPermission, function(req, res){
   var user = req.flash('user')[0];
   var errors = req.flash('errors')[0] || {};
   if(!user){
@@ -57,7 +48,7 @@ router.get('/:username/edit', function(req, res){
 });
 
 // update // 2
-router.put('/:username', function(req, res, next){
+router.put('/:username', util.isLoggedin, checkPermission, function(req, res, next){
   User.findOne({username:req.params.username}) // 2-1
     .select('password') // 2-2
     .exec(function(err, user){
@@ -74,7 +65,7 @@ router.put('/:username', function(req, res, next){
       user.save(function(err, user){
         if(err){
           req.flash('user', req.body);
-          req.flash('errors', parseError(err));
+          req.flash('errors', util.parseError(err));
           return res.redirect('/users/'+req.params.username+'/edit');
         }
         res.redirect('/users/'+user.username);
@@ -83,29 +74,21 @@ router.put('/:username', function(req, res, next){
 });
 
 // destroy
-router.delete('/:username', function(req, res){
-  User.deleteOne({username:req.params.username}, function(err){
-    if(err) return res.json(err);
-    res.redirect('/users');
-  });
-});
+// router.delete('/:username', function(req, res){
+//   User.deleteOne({username:req.params.username}, function(err){
+//     if(err) return res.json(err);
+//     res.redirect('/users');
+//   });
+// });
 
 module.exports = router;
 
-// functions
-function parseError(errors){
-  var parsed = {};
-  if(errors.name == 'ValidationError'){
-    for(var name in errors.errors){
-      var validationError = errors.errors[name];
-      parsed[name] = { message:validationError.message };
-    }
-  }
-  else if(errors.code == '11000' && errors.errmsg.indexOf('username') > 0){
-    parsed.username = { message: 'This username already exists!'};
-  }
-  else{
-    parsed.unhandled = JSON.stringify(errors);
-  }
-  return parsed;
+// private functions
+function checkPermission(req, res, next){
+ User.findOne({username:req.params.username}, function(err, user){
+  if(err) return res.json(err);
+  if(user.id != req.user.id) return util.noPermission(req, res);
+
+  next();
+ });
 }
